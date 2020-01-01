c0d0842b1fea4cc4da361bae05398db7b45f18fd
Fire Chan <cken497@gmail.com>
2020-01-01 13:18:35 +0800

    edit package

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+
 
 const loginPage = require("./routes/front");
 const createPhotoPage = require("./routes/createPhoto");
@@ -9,11 +8,6 @@ const showMap = require("./routes/showMap");
 const app = express();
 
 app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
 
 app.use(express.static(__dirname + "/public"));
 app.use("/bootstrap_css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
